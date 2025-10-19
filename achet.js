11195b3e735476345066d25cf49de144ee886bd1
Nico <107780008+tallerlosojos@users.noreply.github.com>
2025-10-19 16:44:23 -0300

    añadido formulario

--- achet.js
+++ achet.js
@@ -14,13 +14,14 @@ let logotipo;
 function setup() {
     let canvas = createCanvas(400, 200);
           canvas.id('imgCanvas');
+          canvas.parent('p5-canvas-container');
     smooth(10);
 }
 function preload() {
     logotipo = loadImage("data/ltipo1.png");
 }
 function draw() {
-    frameRate(floor(random(1,3)));
+    frameRate(floor(random(1,6)));
     for (let x = 0; x < amountX; x++) {
         for (let y = 0; y < amountY; y++) {
             state[x][y] = int(random(0, 8));
